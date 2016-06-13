2e9af7418d0af9bc867aa2a069ee7f1ddc8cc69e
Kamel Derouiche <kamel.derouiche@gmail.com>
2016-06-13 11:39:11 +0100

    start

--- array/quaternion.go
+++ array/quaternion.go
@@ -1,1 +1,14 @@
 package quaternion
+
+/*
+Quaternion array package
+A Quaternion array is a struct with 4 columns defined as x y z w
+it can be created simply using slice.
+*/
+
+import "math"
+
+//Inverse of quaternion array q
+func Inverse(q) {
+
+}
