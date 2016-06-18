47b1356ef3fa0781c3e4223566937810af227743
K.I.A.Derouiche <kamel.derouiche@gmail.com>
2016-06-18 15:04:35 +0100

    rename directory

---
+++ qarray/quaternion.go
@@ -0,0 +1,62 @@
+package quaternion
+
+/*
+Quaternion array package
+A Quaternion array is a struct with 4 columns defined as x y z w
+it can be created simply using slice.
+*/
+
+import "math"
+
+type qMatrix struct {
+	qMat []float64
+}
+
+//Dot product of a lists of arrays, returns a column array
+func (qm *qMatrix) ArrayDot(q, []float64) {
+
+}
+
+//Inverse of quaternion array q
+func (qm *qMatrix) Inverse(q []float64) []float64 {
+	return q * qm.qMat{-1, -1, -1, 1}
+}
+
+func (qm *qMatrix) Amplitude(q []float64) []float64 {
+
+}
+
+//Normalize quaternion array q or array list to unit quaternions
+func (qm *qMatrix) Norm([]float64) []float64 {
+
+}
+
+//
+func Mul() {
+
+}
+
+//Exponential of a quaternion array
+func (qm *qMatrix) Exp() {
+
+}
+
+//Neprien logarithm of a quaternion array
+func (qm *qMatrix) Ln() {
+
+}
+
+//Real power of a quaternion array
+func (qm *qMatrix) Pow() {
+
+}
+
+//Rotate vector or array of vectors v by quaternion q
+func Rotate(q []float64) {
+
+}
+
+//
+func Toaxisangle() {
+
+}
